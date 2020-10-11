86a5fa553df32b15aca14efa8afdf19daf1e9bf1
gavinleroy <gavinleroy6@gmail.com>
2020-10-11 17:52:52 -0600

    Now I can double nums

--- rust-playground/read_num.rs
+++ rust-playground/read_num.rs
@@ -1,14 +1,17 @@
 use std::io;
 
+fn double(x: u32) -> u32{
+   return x*2; 
+}
+
 fn main() {
     let mut input_text = String::new();
     io::stdin()
-        .read_line(&mut input_text)
-        .expect("failed to read from stdin");
+        .read_line(&mut input_text);
 
     let trimmed = input_text.trim();
     match trimmed.parse::<u32>() {
-        Ok(i) => println!("your integer input: {}", i),
+        Ok(i) => println!("Twice your input is: {}", double(i)),
         Err(..) => println!("this was not an integer: {}", trimmed),
     };
 }
