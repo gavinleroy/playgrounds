c4e1ed9f09255f52325e4ac061ad9faaba41fa7f
gavinleroy <gavinleroy6@gmail.com>
2020-10-29 23:37:55 -0600

    Book init

---
+++ rust-playground/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/gavingray/CodeFiles/playgrounds/rust-playground/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/gavingray/CodeFiles/playgrounds/rust-playground/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ rust-playground/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/gavingray/CodeFiles/playgrounds/rust-playground/hello_cargo/target/debug/hello_cargo: /Users/gavingray/CodeFiles/playgrounds/rust-playground/hello_cargo/src/main.rs
