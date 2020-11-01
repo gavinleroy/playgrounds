0cbc5bf1728c1f0ee8ed13abd5c9088d89654f1e
gavinleroy <gavinleroy6@gmail.com>
2020-10-31 23:28:00 -0600

    simple ownership demo

---
+++ rust-playground/ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+/Users/gavingray/CodeFiles/playgrounds/rust-playground/ownership/target/debug/deps/ownership: src/main.rs
+
+/Users/gavingray/CodeFiles/playgrounds/rust-playground/ownership/target/debug/deps/ownership.d: src/main.rs
+
+src/main.rs:
---
+++ rust-playground/ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/gavingray/CodeFiles/playgrounds/rust-playground/ownership/target/debug/ownership: /Users/gavingray/CodeFiles/playgrounds/rust-playground/ownership/src/main.rs
