02145ffd2dba51a310496218e441ecba5135da8c
gavinleroy <gavinleroy6@gmail.com>
2020-11-06 10:20:38 -0700

    Fixed formatting etc

--- rust-playground/CS6150-HW4-1-d.rs
+++ rust-playground/CS6150-HW4-1-d.rs
@@ -1,5 +1,4 @@
 // Gavin Gray CS 6150 HW4 1.d
-
 use std::vec::Vec;
 use std::f64;
 
@@ -10,7 +9,6 @@ fn fact(n:u32) -> u64 {
   }
   return f;
 }
-
 fn choose(n: u32, k: u32)  -> u64 {
    let mut num:u64 = n as u64;
    for i in 1..k {
@@ -18,23 +16,21 @@ fn choose(n: u32, k: u32)  -> u64 {
    }
    return num / fact(k);
 }
-
 fn prob(j: u32, n: u32) -> f64 {
    (j - 1) as f64 * (n - j) as f64 / choose(n, 3) as f64
 }
-
 fn solve(v: &mut Vec<f64>) {
-    v.push(0.0); 
-    v.push(1.0); 
-    v.push(3.0); 
+    v.push(0.); 
+    v.push(1.); 
+    v.push(3.); 
     for n in 3..=1000 {
         let summation: f64 = (1..n)
-            .fold(0.0, |s: f64, j| s + (prob(j, n) * f64::max(v[(j-1) as usize], v[(n-j) as usize])));
+            .fold(0., |s: f64, j| 
+                  s + (prob(j, n) * 
+                       f64::max(v[(j-1) as usize], v[(n-j) as usize])));
         v.push(n as f64 + summation);
     }
 }
-
-
 fn main() {
     let mut v = Vec::<f64>::new();
     solve(&mut v);
